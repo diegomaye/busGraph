dfc32c8cfe8cafbbc3c0212cd4bdf63d75841ae2
Diego Maye <diego@code4business.com>
2019-07-16 23:57:27 -0300

    Project creation

---
+++ busGraph/busGraph/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  busGraph
+//
+//  Created by diegomaye on 7/16/19.
+//  Copyright © 2019 Diego Rodrigo Pablo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ busGraph2/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  PolynomialCalculator
+//
+//  Created by Diego, Rodrigo & Pablo.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("///////////////////////////////////////////////////////////\n");
+    printf("      ____        __                            _       __\n");
+    printf("     / __ \\____  / __  ______  ____  ____ ___  (_____ _/ /\n");
+    printf("    / /_/ / __ \\/ / / / / __ \\/ __ \\/ __ `__ \\/ / __ `/ /\n");
+    printf("   / ____/ /_/ / / /_/ / / / / /_/ / / / / / / / /_/ / /\n");
+    printf("  /_/    \\____/_/\\__, /_/ /_/\\____/_/ /_/ /_/_/\\__,_/_/\n");
+    printf("     ______     /____/        __      __\n");
+    printf("    / ________ _/ _______  __/ ____ _/ /_____  _____\n");
+    printf("   / /   / __ `/ / ___/ / / / / __ `/ __/ __ \\/ ___/\n");
+    printf("  / /___/ /_/ / / /__/ /_/ / / /_/ / /_/ /_/ / /\n");
+    printf("  \\____/\\__,_/_/\\___/\\__,_/_/\\__,_/\\__/\\____/_/\n");
+    printf("\n");
+    printf("    Bienvenido a Polynomial Calculator, el sitema permite crear\n");
+    printf("sumar, multiplicar, evaluar y guardar polinomios, ingrese 'ayuda'\n");
+    printf("para mas informacion sobre los comandos.\n");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////\n");
+
+}
